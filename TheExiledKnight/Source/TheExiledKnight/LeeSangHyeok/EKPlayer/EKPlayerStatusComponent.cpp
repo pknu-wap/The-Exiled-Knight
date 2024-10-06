23080a381f7fd9c0569e521ee024212de84c0432
KaneBigNose <imddongs@naver.com>
2024-10-06 16:38:49 +0900

    Feat: Add Player Input
    Add Player Class
    Add PlayerController Class
    Add Player (Move, Jump, Look)
    Add PlayerStatusComponent Class

---
+++ TheExiledKnight/Source/TheExiledKnight/LeeSangHyeok/EKPlayer/EKPlayerStatusComponent.cpp
@@ -0,0 +1,28 @@
+// Made by Somalia Pirate
+
+
+#include "EKPlayerStatusComponent.h"
+
+UEKPlayerStatusComponent::UEKPlayerStatusComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+	
+}
+
+
+void UEKPlayerStatusComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
+
+
+void UEKPlayerStatusComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	
+}
+
