e8a626e4c8d1a4d45697bf08501f45d848e7d12a
KaneBigNose <imddongs@naver.com>
2024-10-10 21:46:30 +0900

    Feat: add weapon equip animation, add sprint
    add equip socket
    add weapon equip, unequip method
    add sprint input, method

--- TheExiledKnight/Source/TheExiledKnight/Player/EKPlayer/EKPlayerController.cpp
+++ TheExiledKnight/Source/TheExiledKnight/Player/EKPlayer/EKPlayerController.cpp
@@ -9,14 +9,16 @@
 #include "EKPlayer.h"
 #include "Kismet/KismetMathLibrary.h"
 #include "GameFramework/PawnMovementComponent.h"
+#include "../Weapon/GreatSword.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 AEKPlayerController::AEKPlayerController(const FObjectInitializer& ObjectInitializer)
 	:Super(ObjectInitializer)
 {
-	ConstructorHelpers::FObjectFinder<UInputMappingContext> IMCGreatSwordFinder(TEXT("/Game/EKPlayer/Input/IMC_EK_GreatSword"));
+	ConstructorHelpers::FObjectFinder<UInputMappingContext> IMCGreatSwordFinder(TEXT("/Game/EKPlayer/Input/GreatSword/IMC_EK_GreatSword"));
 	if (IMCGreatSwordFinder.Succeeded())
 	{
-		IMCGreatSword = IMCGreatSwordFinder.Object;
+		IMCDefault = IMCGreatSwordFinder.Object;
 	}
 
 	ConstructorHelpers::FObjectFinder<UInputAction> IAMoveFinder(TEXT("/Game/EKPlayer/Input/IA_EK_Move"));
@@ -37,6 +39,18 @@ AEKPlayerController::AEKPlayerController(const FObjectInitializer& ObjectInitial
 		IAJump = IAJumpFinder.Object;
 	}
 
+	ConstructorHelpers::FObjectFinder<UInputAction> IAWeaponChangeFinder(TEXT("/Game/EKPlayer/Input/IA_EK_WeaponChange"));
+	if (IAWeaponChangeFinder.Succeeded())
+	{
+		IAWeaponChange = IAWeaponChangeFinder.Object;
+	}
+
+	ConstructorHelpers::FObjectFinder<UInputAction> IASprintAndDodgeFinder(TEXT("/Game/EKPlayer/Input/IA_EK_SprintAndDodge"));
+	if (IASprintAndDodgeFinder.Succeeded())
+	{
+		IASprintAndDodge = IASprintAndDodgeFinder.Object;
+	}
+
 	ConstructorHelpers::FObjectFinder<UInputAction> IAGreatSwordAttackFinder(TEXT("/Game/EKPlayer/Input/GreatSword/IA_EK_GreatSword_Attack"));
 	if (IAGreatSwordAttackFinder.Succeeded())
 	{
@@ -48,6 +62,18 @@ AEKPlayerController::AEKPlayerController(const FObjectInitializer& ObjectInitial
 	{
 		GreatSwordAttackAnim = GreatSwordAttackAnimFinder.Object;
 	}
+
+	ConstructorHelpers::FObjectFinder<UAnimMontage> GreatSwordEquipAnimFinder(TEXT("/Game/EKPlayer/Animation/GreatSword/Equip/EKPlayer_Equip_GreatSword_Montage"));
+	if (GreatSwordEquipAnimFinder.Succeeded())
+	{
+		GreatSwordEquipAnim = GreatSwordEquipAnimFinder.Object;
+	}
+
+	ConstructorHelpers::FObjectFinder<UAnimMontage> GreatSwordUnEquipAnimFinder(TEXT("/Game/EKPlayer/Animation/GreatSword/Equip/EKPlayer_UnEquip_GreatSword_Montage"));
+	if (GreatSwordUnEquipAnimFinder.Succeeded())
+	{
+		GreatSwordUnEquipAnim = GreatSwordUnEquipAnimFinder.Object;
+	}
 }
 
 void AEKPlayerController::BeginPlay()
@@ -58,7 +84,7 @@ void AEKPlayerController::BeginPlay()
 
 	if (auto* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
 	{
-		Subsystem->AddMappingContext(IMCGreatSword, 0);
+		Subsystem->AddMappingContext(IMCDefault, 0);
 	}
 }
 
@@ -71,6 +97,14 @@ void AEKPlayerController::SetupInputComponent()
 		EnhancedInputComponent->BindAction(IAMove, ETriggerEvent::Triggered, this, &ThisClass::MoveAction);
 		EnhancedInputComponent->BindAction(IALook, ETriggerEvent::Triggered, this, &ThisClass::LookAction);
 		EnhancedInputComponent->BindAction(IAJump, ETriggerEvent::Triggered, this, &ThisClass::JumpAction);
+
+		EnhancedInputComponent->BindAction(IAWeaponChange, ETriggerEvent::Triggered, this, &ThisClass::WeaponChangeAction);
+
+		//EnhancedInputComponent->BindAction(IASprintAndDodge, ETriggerEvent::Started, this, &ThisClass::SprintAndDodgeAction);
+		EnhancedInputComponent->BindAction(IASprintAndDodge, ETriggerEvent::Triggered, this, &ThisClass::SprintAndDodgeAction);
+		EnhancedInputComponent->BindAction(IASprintAndDodge, ETriggerEvent::Completed, this, &ThisClass::SprintAndDodgeRelease);
+		EnhancedInputComponent->BindAction(IASprintAndDodge, ETriggerEvent::Canceled, this, &ThisClass::SprintAndDodgeRelease);
+
 		EnhancedInputComponent->BindAction(IAGreatSwordAttack, ETriggerEvent::Triggered, this, &ThisClass::GreatSwordAttackAction);
 	}
 }
@@ -79,24 +113,24 @@ void AEKPlayerController::PlayerTick(float DeltaTime)
 {
 	Super::PlayerTick(DeltaTime);
 
-	if (EKPlayer->GetPlayerCurrentState() == EEKPlayerBehaviorState::Jump && !EKPlayer->GetMovementComponent()->IsFalling())
+	/*if (EKPlayer->GetPlayerCurrentState() == EEKPlayerBehaviorState::Jump && !EKPlayer->GetMovementComponent()->IsFalling())
 	{
 		EKPlayer->SetPlayerCurrentState(EEKPlayerBehaviorState::Idle);
-	}
+	}*/
 }
 
 void AEKPlayerController::MoveAction(const FInputActionValue& InputValue)
 {
-	if (EKPlayer->GetPlayerCurrentState() == EEKPlayerBehaviorState::Attack)
+	/*if (EKPlayer->GetPlayerCurrentState() == EEKPlayerBehaviorState::Attack)
 	{
 		return;
-	}
+	}*/
 
 	FVector2D MovementVector = InputValue.Get<FVector2D>();
 
-	if (MovementVector.X == 0 && MovementVector.Y == 0)
+	if (MovementVector.X != 0 || MovementVector.Y != 0)
 	{
-		EKPlayer->SetPlayerCurrentState(EEKPlayerBehaviorState::Move);
+		//EKPlayer->SetPlayerCurrentState(EEKPlayerBehaviorState::Move);
 
 		FRotator Rotator = GetControlRotation();
 
@@ -114,7 +148,7 @@ void AEKPlayerController::MoveAction(const FInputActionValue& InputValue)
 	}
 	else
 	{
-		EKPlayer->SetPlayerCurrentState(EEKPlayerBehaviorState::Idle);
+		//EKPlayer->SetPlayerCurrentState(EEKPlayerBehaviorState::Idle);
 	}
 }
 
@@ -128,24 +162,59 @@ void AEKPlayerController::LookAction(const FInputActionValue& InputValue)
 
 void AEKPlayerController::JumpAction(const FInputActionValue& InputValue)
 {
-	if (EKPlayer->GetPlayerCurrentState() == EEKPlayerBehaviorState::Attack)
+	/*if (EKPlayer->GetPlayerCurrentState() == EEKPlayerBehaviorState::Attack)
 	{
 		return;
 	}
 
-	EKPlayer->SetPlayerCurrentState(EEKPlayerBehaviorState::Jump);
+	EKPlayer->SetPlayerCurrentState(EEKPlayerBehaviorState::Jump);*/
 
 	EKPlayer->Jump();
 }
 
+void AEKPlayerController::WeaponChangeAction(const FInputActionValue& InputValue)
+{
+	if (!bIsEquipGreatSword && GreatSwordEquipAnim)
+	{
+		EKPlayer->PlayAnimMontage(GreatSwordEquipAnim);
+		bIsEquipGreatSword = true;
+	}
+	else if (bIsEquipGreatSword && GreatSwordUnEquipAnim)
+	{
+		EKPlayer->PlayAnimMontage(GreatSwordUnEquipAnim);
+		bIsEquipGreatSword = false;
+	}
+}
+
+void AEKPlayerController::SprintAndDodgeAction(const FInputActionValue& InputValue)
+{
+	if (EKPlayer)
+	{
+		EKPlayer->GetCharacterMovement()->MaxWalkSpeed = 600;
+	}
+}
+
+void AEKPlayerController::SprintAndDodgeRelease(const FInputActionValue& InputValue)
+{
+	if (EKPlayer)
+	{
+		EKPlayer->GetCharacterMovement()->MaxWalkSpeed = 300;
+	}
+}
+
 void AEKPlayerController::GreatSwordAttackAction(const FInputActionValue& InputValue)
 {
-	if (EKPlayer->GetPlayerCurrentState() != EEKPlayerBehaviorState::Idle)
+	/*if (EKPlayer->GetPlayerCurrentState() != EEKPlayerBehaviorState::Idle)
 	{
 		return;
 	}
 
-	EKPlayer->SetPlayerCurrentState(EEKPlayerBehaviorState::Attack);
+	EKPlayer->SetPlayerCurrentState(EEKPlayerBehaviorState::Attack);*/
+
+	if (!bIsEquipGreatSword)
+	{
+		return;
+	}
 
 	if (GreatSwordAttackAnim)
 	{
