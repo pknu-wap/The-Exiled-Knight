71c4be9189879bb6728f6d709c00b7c09ba0e230
KaneBigNose <imddongs@naver.com>
2024-10-11 22:06:48 +0900

    Feat: Weapon Change 기능 추가
    enum equip weapon 클래스 추가
    greatsword와 spear의 상위 클래스 추가
    spear 장착 장착헤제 몽타주 추가 및 notify 설정
    테스트용 무기 장착 코드 추가

---
+++ TheExiledKnight/Source/TheExiledKnight/Player/EKPlayerAnimation/GreatSword/EquipGreatSword.cpp
@@ -0,0 +1,36 @@
+// Made by Somalia Pirate
+
+
+#include "EquipGreatSword.h"
+#include "../../EKPlayer/EKPlayer.h"
+#include "../../EKPlayer/EKPlayerController.h"
+#include "../../Weapon/GreatSword.h"
+
+void UEquipGreatSword::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	Super::Notify(MeshComp, Animation);
+
+	if (MeshComp && MeshComp->GetOwner())
+	{
+		AActor* OwnerActor = MeshComp->GetOwner();
+
+		EKPlayer = Cast<AEKPlayer>(OwnerActor);
+		if (EKPlayer)
+		{
+			EKPlayerController = Cast<AEKPlayerController>(EKPlayer->GetController());
+			if (EKPlayerController)
+			{
+				if (EKPlayerController->bIsEquipGreatSword == false)
+				{
+					EKPlayer->AttachGreatSwordToHandSocket(EKPlayer->GetCurrentWeapon());
+					EKPlayerController->bIsEquipGreatSword = true;
+				}
+				else if (EKPlayerController->bIsEquipGreatSword == true)
+				{
+					EKPlayer->AttachGreatSwordToEquipSocket(EKPlayer->GetCurrentWeapon());
+					EKPlayerController->bIsEquipGreatSword = false;
+				}
+			}
+		}
+	}
+}
---
+++ TheExiledKnight/Source/TheExiledKnight/Player/Weapon/EKPlayerWeapon.cpp
@@ -0,0 +1,23 @@
+// Made by Somalia Pirate
+
+
+#include "EKPlayerWeapon.h"
+
+AEKPlayerWeapon::AEKPlayerWeapon()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AEKPlayerWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AEKPlayerWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
