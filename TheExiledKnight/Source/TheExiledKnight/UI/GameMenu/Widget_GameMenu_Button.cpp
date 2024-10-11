866fb6f7b3bbcf53e170388fc38c755e9ab570f9
HyeonJun-Jung <jhj123p@gmail.com>
2024-10-11 15:45:00 +0900

    Feat : GameMenu 추가
    - UI Reflector를 통해 Visible인 Container 탐색 -> Not Hit Test로 변경
    -

---
+++ TheExiledKnight/Source/TheExiledKnight/UI/GameMenu/Widget_GameMenu_Button.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/GameMenu/Widget_GameMenu_Button.h"
+#include "Components/Button.h"
+#include "UI/UISubsystem.h"
+#include "EKGameplayTags.h"
+
+void UWidget_GameMenu_Button::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	Button_Menu->OnClicked.AddUniqueDynamic(this, &UWidget_GameMenu_Button::MenuButtonClicked);
+}
+
+void UWidget_GameMenu_Button::MenuButtonClicked()
+{
+	UUISubsystem* UIManager = GetGameInstance()->GetSubsystem<UUISubsystem>();
+	if (!UIManager) return;
+
+	switch (MenuCategory)
+	{
+	case EGameMenuCategory::Inventory:
+	{
+		UIManager->SetWidgetVisibility(FEKGameplayTags::Get().UI_Widget_GameMenu_Inventory, ESlateVisibility::Visible);
+		break;
+	}
+	case EGameMenuCategory::Equipment:
+	{
+		UIManager->SetWidgetVisibility(FEKGameplayTags::Get().UI_Widget_GameMenu_Equipment, ESlateVisibility::Visible);
+		break;
+	}
+	case EGameMenuCategory::Option:
+	{
+		break;
+	}
+	case EGameMenuCategory::Quit:
+	{
+
+		break;
+	}
+	default:
+		break;
+	}
+}
