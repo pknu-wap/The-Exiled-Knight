dd05e10f15dfb8324fcfa722119fe5eac4004f7f
KaneBigNose <imddongs@naver.com>
2024-10-13 20:29:57 +0900

    Feat: Great Sword Attack Notify 추가

---
+++ TheExiledKnight/Source/TheExiledKnight/Player/EKPlayerNotify/GreatSword/GreatSwordAttack1.cpp
@@ -0,0 +1,21 @@
+// Made by Somalia Pirate
+
+#include "GreatSwordAttack1.h"
+
+void UGreatSwordAttack1::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+
+}
+
+void UGreatSwordAttack1::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, FrameDeltaTime, EventReference);
+
+}
+
+void UGreatSwordAttack1::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+
+}
