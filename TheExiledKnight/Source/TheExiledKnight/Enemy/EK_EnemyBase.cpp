6477ba701052b27f3890ba804c6061bab6eccbed
hallow0312 <leejaehyun0312@naver.com>
2024-10-27 03:38:00 +0900

    Remove:BPIDamageable Add: EK_EnemyBase

---
+++ TheExiledKnight/Source/TheExiledKnight/Enemy/EK_EnemyBase.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EK_EnemyBase.h"
+#include "EK_EnemyStatusComponent.h"
+// Sets default values
+AEK_EnemyBase::AEK_EnemyBase()
+{
+	EnemyStat = CreateDefaultSubobject<UEK_EnemyStatusComponent>(TEXT("EnemyStat"));
+}
+
+
+
+
+
+
