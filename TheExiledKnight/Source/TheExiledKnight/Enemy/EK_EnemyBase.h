b13739912a92947a71f0a030e986a228062fa07d
hallow0312 <leejaehyun0312@naver.com>
2024-10-27 03:38:00 +0900

    Remove:BPIDamageable Add: EK_EnemyBase

---
+++ TheExiledKnight/Source/TheExiledKnight/Enemy/EK_EnemyBase.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "EK_EnemyBase.generated.h"
+
+UCLASS()
+class THEEXILEDKNIGHT_API AEK_EnemyBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AEK_EnemyBase(); 
+
+protected:
+	// Called when the game starts or when spawned
+
+
+public:	
+	UPROPERTY(VisibleAnywhere, Category = Stat)
+	class UEK_EnemyStatusComponent* EnemyStat;
+
+};
