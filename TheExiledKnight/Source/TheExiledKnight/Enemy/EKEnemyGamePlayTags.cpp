2861d43fd4e18624c652ff0b7d6b3dfe057c6d69
HyeonJun-Jung <jhj123p@gmail.com>
2024-10-28 20:46:00 +0900

    Feat : Build UMG 추가 & GameplayTag 중복 수정 & uproject 버그 수정

--- TheExiledKnight/Source/TheExiledKnight/Enemy/EKEnemyGamePlayTags.cpp
+++ TheExiledKnight/Source/TheExiledKnight/Enemy/EKEnemyGamePlayTags.cpp
@@ -7,9 +7,9 @@ namespace EKPlayerGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(EKEnemy_State_Idle, "EKEnemy.State.Idle");
 	UE_DEFINE_GAMEPLAY_TAG(EKEnemy_State_Walk, "EKEnemy.State.Walk");
 	UE_DEFINE_GAMEPLAY_TAG(EKEnemy_State_Sprint, "EKEnemy.State.Sprint");
-	UE_DEFINE_GAMEPLAY_TAG(EKPlayer_State_Attack, "EKEnemy.State.Attack");
-	UE_DEFINE_GAMEPLAY_TAG(EKPlayer_State_Chase, "EKEnemy.State.Chase");
-	UE_DEFINE_GAMEPLAY_TAG(EKPlayer_State_Stun, "EKEnemy.State.Stun");
+	UE_DEFINE_GAMEPLAY_TAG(EKEnemy_State_Attack, "EKEnemy.State.Attack");
+	UE_DEFINE_GAMEPLAY_TAG(EKEnemy_State_Chase, "EKEnemy.State.Chase");
+	UE_DEFINE_GAMEPLAY_TAG(EKEnemy_State_Stun, "EKEnemy.State.Stun");
 
 
 }
