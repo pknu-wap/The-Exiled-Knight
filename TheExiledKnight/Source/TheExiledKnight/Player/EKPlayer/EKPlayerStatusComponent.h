ce56d4239d711027687f3fd47cbfee96aba9bf49
HyeonJun-Jung <126565285+HyeonJun-Jung@users.noreply.github.com>
2024-10-28 20:32:31 +0900

    Revert "Pull Request Feature/AIEnemy"

---
+++ TheExiledKnight/Source/TheExiledKnight/Player/EKPlayer/EKPlayerStatusComponent.h
@@ -0,0 +1,137 @@
+// Made by Somalia Pirate
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "EKPlayerStatusComponent.generated.h"
+
+#define PlayerMaxHp 10000
+#define PlayerMaxMp 10000
+#define PlayerMaxStamina 10000
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class UEKPlayerStatusComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEKPlayerStatusComponent();
+
+public:
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+public:
+	uint32 GetMaxHp();
+	uint32 GetHp();
+	uint32 GetMaxMp();
+	uint32 GetMp();
+	uint32 GetMaxStamina();
+	uint32 GetStamina();
+
+	void SetMaxHp(int32 SetData);
+	void SetHp(int32 SetData);
+	void SetMaxMp(int32 SetData);
+	void SetMp(int32 SetData);
+	void SetMaxStamina(int32 SetData);
+	void SetStamina(int32 SetData);
+
+public:
+	uint32 GetPlayerDefaultDamage();
+	void SetPlayerDefaultDamage();
+	uint32 GetPlayerFinalDamage();
+	void SetPlayerFinalDamage();
+
+	void LevelUp();
+	void LevelUpVitality();
+	void LevelUpMental();
+	void LevelUpEndurance();
+	void LevelUpStrength();
+	void LevelUpAbility();
+	void LevelUpIntelligence();
+
+	// About Weapon Attack Combo
+	uint32 GetGreatSwordCombo();
+	void SetGreatSwordCombo();
+	void ResetGreatSwordCombo();
+
+	uint32 GetSpearCombo();
+	void SetSpearCombo();
+	void ResetSpearCombo();
+
+	uint32 GetStaffCombo();
+	void SetStaffCombo();
+	void ResetStaffCombo();
+
+public:
+	bool bCanStaminaRecovery = true;
+
+protected:
+	UPROPERTY()
+	TObjectPtr<class AEKPlayer> EKPlayer;
+
+	UPROPERTY()
+	TObjectPtr<class AEKPlayerController> EKPlayerController;
+
+protected:
+	// Basic status
+	UPROPERTY(VisibleAnywhere, Category = "Statment|Basic")
+	uint32 MaxHp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Statment|Basic")
+	uint32 Hp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Statment|Basic")
+	uint32 MaxMp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Statment|Basic")
+	uint32 Mp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Statment|Basic")
+	uint32 MaxStamina;
+
+	UPROPERTY(VisibleAnywhere, Category = "Statment|Basic")
+	uint32 Stamina;
+
+	UPROPERTY(VisibleAnywhere, Category = "Statment|Basic")
+	uint32 DefaultDamage;
+
+	UPROPERTY(VisibleAnywhere, Category = "Statment|Basic")
+	uint32 FinalDamage;
+
+protected:
+	// Upgrade status
+	UPROPERTY(VisibleAnywhere, Category = "Statment|Upgrade")
+	uint32 MaxLevel;
+
+	UPROPERTY(VisibleAnywhere, Category = "Statment|Upgrade")
+	uint32 Level;
+
+	UPROPERTY(VisibleAnywhere, Category = "Statment|Upgrade")
+	uint32 Vitality;
+
+	UPROPERTY(VisibleAnywhere, Category = "Statment|Upgrade")
+	uint32 Mental;
+
+	UPROPERTY(VisibleAnywhere, Category = "Statment|Upgrade")
+	uint32 Endurance;
+
+	UPROPERTY(VisibleAnywhere, Category = "Statment|Upgrade")
+	uint32 Strength;
+
+	UPROPERTY(VisibleAnywhere, Category = "Statment|Upgrade")
+	uint32 Ability;
+
+	UPROPERTY(VisibleAnywhere, Category = "Statment|Upgrade")
+	uint32 Intelligence;
+
+protected:
+	UPROPERTY()
+	uint32 GreatSwordCombo = 1;
+
+	UPROPERTY()
+	uint32 SpearCombo = 1;
+
+	UPROPERTY()
+	uint32 StaffCombo = 1;
+};
---
+++ TheExiledKnight/Source/TheExiledKnight/Player/EKPlayerNotify/GreatSword/GreatSwordAttack3.cpp
@@ -0,0 +1,36 @@
+// Made by Somalia Pirate
+
+#include "GreatSwordAttack3.h"
+#include "../../EKPlayer/EKPlayer.h"
+#include "../../EKPlayer/EKPlayerController.h"
+
+void UGreatSwordAttack3::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+
+	if (EKPlayer = Cast<AEKPlayer>(MeshComp->GetOwner()))
+	{
+		EKPlayerController = Cast<AEKPlayerController>(EKPlayer->GetController());
+	}
+
+	if (EKPlayerController)
+	{
+		EKPlayerController->SetAttackEndTimer(2.7f);
+	}
+}
+
+void UGreatSwordAttack3::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, FrameDeltaTime, EventReference);
+
+}
+
+void UGreatSwordAttack3::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+
+	if (EKPlayerController)
+	{
+		EKPlayerController->SetAttackNextAndTimer();
+	}
+}
