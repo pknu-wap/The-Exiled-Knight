3d408b5aee1989c74e973bea21916d46aa1958fb
KaneBigNose <imddongs@naver.com>
2024-10-29 20:59:28 +0900

    Feat: SitDown 추가, Fix: Dodge 수정
    SitDown input 추가 및 애니메이션 추가
    SitDown 전용 걷기 애니메이션 추가
    Dodge 발동 시 플레이어의 방향 입력에 따라 구르던 방향을 전환하는 현상 수정

--- TheExiledKnight/Source/TheExiledKnight/Player/EKPlayerGameplayTags.cpp
+++ TheExiledKnight/Source/TheExiledKnight/Player/EKPlayerGameplayTags.cpp
@@ -5,6 +5,8 @@
 namespace EKPlayerGameplayTags
 {
 	// About EKPlayer Current State
+	UE_DEFINE_GAMEPLAY_TAG(EKPlayer_State_BattleState, "EKPlayer.State.Battle");
+
 	UE_DEFINE_GAMEPLAY_TAG(EKPlayer_State_Move, "EKPlayer.State.Move");
 	UE_DEFINE_GAMEPLAY_TAG(EKPlayer_State_Look, "EKPlayer.State.Look");
 	UE_DEFINE_GAMEPLAY_TAG(EKPlayer_State_Sprint, "EKPlayer.State.Sprint");
@@ -13,4 +15,5 @@ namespace EKPlayerGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(EKPlayer_State_Defense, "EKPlayer.State.Defense");
 	UE_DEFINE_GAMEPLAY_TAG(EKPlayer_State_Dodge, "EKPlayer.State.Dodge");
 	UE_DEFINE_GAMEPLAY_TAG(EKPlayer_State_UseItem, "EKPlayer.State.UseItem");
+	UE_DEFINE_GAMEPLAY_TAG(EKPlayer_State_SitDown, "EKPlayer.State.SitDown");
 }
