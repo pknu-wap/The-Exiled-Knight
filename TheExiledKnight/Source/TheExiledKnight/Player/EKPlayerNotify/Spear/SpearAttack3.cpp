6f2f31b49bd234310dbc8f1db16451b66f62b9b6
KaneBigNose <imddongs@naver.com>
2024-10-29 19:11:12 +0900

    Feat: GameplayTags 활용
    GameplayTags의 Container를 이용하여 플레이어의 상태를 정의하고 특정 행동을 제한시킴

--- TheExiledKnight/Source/TheExiledKnight/Player/EKPlayerNotify/Spear/SpearAttack3.cpp
+++ TheExiledKnight/Source/TheExiledKnight/Player/EKPlayerNotify/Spear/SpearAttack3.cpp
@@ -3,6 +3,7 @@
 #include "SpearAttack3.h"
 #include "../../EKPlayer/EKPlayer.h"
 #include "../../EKPlayer/EKPlayerController.h"
+#include "../../EKPlayerGameplayTags.h"
 
 void USpearAttack3::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
 {
@@ -32,5 +33,10 @@ void USpearAttack3::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBas
 	if (EKPlayerController)
 	{
 		EKPlayerController->SetAttackNextAndTimer();
+	}
+
+	if (EKPlayer)
+	{
+		EKPlayer->EKPlayerStateContainer.RemoveTag(EKPlayerGameplayTags::EKPlayer_State_Attack);
 	}
 }
