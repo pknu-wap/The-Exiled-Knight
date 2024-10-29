9dc7a8c5399adee35dd1a713eb56f6c5462ea3e5
WWjj16 <lds062626@naver.com>
2024-10-30 00:51:35 +0900

    Add Item Struct & Enum, Inventory (c++)
    인벤토리 관련 구조체 및 열거형 추가
    인벤토리 시스템 c++

--- TheExiledKnight/Source/TheExiledKnight/TheExiledKnight.Build.cs
+++ TheExiledKnight/Source/TheExiledKnight/TheExiledKnight.Build.cs
@@ -9,5 +9,7 @@ public class TheExiledKnight : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
-	}
+
+        PrivateIncludePaths.Add("TheExiledKnight");
+    }
 }
