9cc2626dcd4c51a1ebac1021e72ab65a0a1340cf
WWjj16 <lds062626@naver.com>
2024-10-30 00:51:35 +0900

    Add Item Struct & Enum, Inventory (c++)
    인벤토리 관련 구조체 및 열거형 추가
    인벤토리 시스템 c++

---
+++ TheExiledKnight/Source/TheExiledKnight/Structs/ItemStruct.h
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EKEnums.h"
+#include "ItemStruct.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct FItemStruct : public FTableRowBase
+{
+    GENERATED_BODY()
+
+    FItemStruct() {};
+    FItemStruct(const FItemStruct& Other)
+    {
+        ID = Other.ID;
+        Name = Other.Name;
+        Description = Other.Description;
+        ItemCategory = Other.ItemCategory;
+        Icon = Other.Icon;
+        MaxStackSize = Other.MaxStackSize;
+        ItemValue = Other.ItemValue;
+        StaticMesh = Other.StaticMesh;
+        SkeletonMesh = Other.SkeletonMesh;
+    }
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    uint8 ID = 0;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FName Name;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FString Description;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    EItemCategory ItemCategory;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UTexture2D* Icon;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    uint8 MaxStackSize;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    int ItemValue;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UStaticMesh* StaticMesh;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    USkeletalMesh* SkeletonMesh;
+};
