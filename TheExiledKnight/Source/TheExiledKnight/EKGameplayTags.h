60427c9eed2d252f1eb06cababaae70e4af7c4a9
HyeonJun-Jung <jhj123p@gmail.com>
2024-10-30 01:49:54 +0900

    Feat : Setting 및 장비 선택 창 추가

--- TheExiledKnight/Source/TheExiledKnight/EKGameplayTags.h
+++ TheExiledKnight/Source/TheExiledKnight/EKGameplayTags.h
@@ -13,14 +13,22 @@ public:
 
 	static void InitializeNativeGameplayTags();
 
-	// UI
+	////////// UI /////////
+
+	// Layer
 	FGameplayTag UI_Layer_Game;			// Player Status, Weapon Slots . . .
 	FGameplayTag UI_Layer_GameMenu;		// Inventory, Quest . . .
 	FGameplayTag UI_Layer_Menu;			// Option . . .
 
-	FGameplayTag UI_Widget_GameMenu_GameMenu;	// GameMenu
-	FGameplayTag UI_Widget_GameMenu_Inventory;	// Inventory
-	FGameplayTag UI_Widget_GameMenu_Equipment;	// Equipment
+	// Widget
+	FGameplayTag UI_Widget_GameMenu_GameMenu;		// GameMenu
+	FGameplayTag UI_Widget_GameMenu_Inventory;		// Inventory
+	FGameplayTag UI_Widget_GameMenu_Equipment;		// Equipment
+	FGameplayTag UI_Widget_GameMenu_EquipSelect;	// Equip Select
+
+	FGameplayTag UI_Widget_Game_Status;				// Status
+	FGameplayTag UI_Widget_Game_QuickSlots;			// QuickSlots
+
 
 private:
 	static FEKGameplayTags GameplayTags;
