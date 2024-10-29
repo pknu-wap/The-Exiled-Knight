13148181702cbe6941d048001cd74c3d395db866
HyeonJun-Jung <jhj123p@gmail.com>
2024-10-30 01:49:54 +0900

    Feat : Setting 및 장비 선택 창 추가

---
+++ TheExiledKnight/Source/TheExiledKnight/UI/Equipment/Widget_EquipSelect_ContentList.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Widget_EquipSelect_ContentList.generated.h"
+
+class UListView;
+
+UCLASS()
+class THEEXILEDKNIGHT_API UWidget_EquipSelect_ContentList : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, EditAnywhere)
+	UListView* ContentList;
+};
