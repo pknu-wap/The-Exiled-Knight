c234dd9317512c260fd15a93ffe0567d937c5858
HyeonJun-Jung <jhj123p@gmail.com>
2024-10-30 17:52:45 +0900

    Feat : 장비창 선택 UI 추가

--- TheExiledKnight/Source/TheExiledKnight/UI/Equipment/Widget_EquipSelect_ContentList.h
+++ TheExiledKnight/Source/TheExiledKnight/UI/Equipment/Widget_EquipSelect_ContentList.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "EKEnums.h"
 #include "Widget_EquipSelect_ContentList.generated.h"
 
 class UListView;
@@ -12,6 +13,9 @@ UCLASS()
 class THEEXILEDKNIGHT_API UWidget_EquipSelect_ContentList : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	void UpdateContents(EItemCategory Category);
 
 public:
 	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, EditAnywhere)
