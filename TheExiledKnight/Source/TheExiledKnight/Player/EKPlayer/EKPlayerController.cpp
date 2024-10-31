0dea419aaeafa58e6b31c9cc0f86dbe0ac17cfca
KaneBigNose <imddongs@naver.com>
2024-11-01 03:21:06 +0900

    Feat: 내용없음

--- TheExiledKnight/Source/TheExiledKnight/Player/EKPlayer/EKPlayerController.cpp
+++ TheExiledKnight/Source/TheExiledKnight/Player/EKPlayer/EKPlayerController.cpp
@@ -16,6 +16,9 @@
 #include "EKPlayerStatusComponent.h"
 #include "../EKPlayerGameplayTags.h"
 #include "Components/InventoryComponent.h"
+#include "UI/UISubsystem.h"
+#include "Blueprint/UserWidget.h"
+#include "EKGameplayTags.h"
 
 AEKPlayerController::AEKPlayerController(const FObjectInitializer& ObjectInitializer)
 	:Super(ObjectInitializer)
