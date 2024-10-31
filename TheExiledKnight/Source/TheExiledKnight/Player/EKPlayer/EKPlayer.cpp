a0419fbdbd5e49bc9de8b370ce69449a702cfdcb
KaneBigNose <imddongs@naver.com>
2024-11-01 01:17:55 +0900

    Fix: 주석 제거

--- TheExiledKnight/Source/TheExiledKnight/Player/EKPlayer/EKPlayer.cpp
+++ TheExiledKnight/Source/TheExiledKnight/Player/EKPlayer/EKPlayer.cpp
@@ -130,7 +130,7 @@ void AEKPlayer::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 	// Test
-	GEngine->AddOnScreenDebugMessage(-1, 0.1f, FColor::Cyan, FString::Printf(TEXT("HP : %d / %d"), PlayerStatusComponent->GetHp(), PlayerStatusComponent->GetMaxHp()));
+	//GEngine->AddOnScreenDebugMessage(-1, 0.1f, FColor::Cyan, FString::Printf(TEXT("HP : %d / %d"), PlayerStatusComponent->GetHp(), PlayerStatusComponent->GetMaxHp()));
 	//GEngine->AddOnScreenDebugMessage(-1, 0.1f, FColor::Cyan, FString::Printf(TEXT("MP : %d / %d"), PlayerStatusComponent->GetMp(), PlayerStatusComponent->GetMaxMp()));
 	//GEngine->AddOnScreenDebugMessage(-1, 0.1f, FColor::Cyan, FString::Printf(TEXT("Stamina : %d / %d"), PlayerStatusComponent->GetStamina(), PlayerStatusComponent->GetMaxStamina()));
 }
