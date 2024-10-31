d287c13da9dce4db64be71d90558b62124c5d65d
WWjj16 <lds062626@naver.com>
2024-10-31 23:21:47 +0900

    Feature/Inventory
    인벤토리 서브시스템 코드 추가 (ID 정보로 행 데이터 접근)
    상호작용 입력 키 설정

--- TheExiledKnight/Source/TheExiledKnight/Subsystems/InventorySubsystem.cpp
+++ TheExiledKnight/Source/TheExiledKnight/Subsystems/InventorySubsystem.cpp
@@ -6,4 +6,34 @@
 UInventorySubsystem::UInventorySubsystem()
 {
 	ItemDB = LoadObject<UDataTable>(this, TEXT("/Script/Engine.DataTable'/Game/TheExiledKnight/Inventory/DataTables/DT_Item.DT_Item'"));
+	
+	if (ItemDB == nullptr)
+		return;
+
+	uint8 ID = 0;
+
+	for (auto RowName : ItemDB->GetRowNames())
+	{
+		ID++;
+		FItemStruct* ItemInfo = ItemDB->FindRow<FItemStruct>(RowName, TEXT("GetItemRow"));
+		ItemDictionary.Add(ID, *ItemInfo);
+	}
+}
+
+FItemStruct* UInventorySubsystem::GetItemRow(FName RowName)
+{
+	return ItemDB->FindRow<FItemStruct>(RowName, TEXT("GetItemRow"));
+}
+
+FItemStruct* UInventorySubsystem::GetItemInfo(uint8 ID)
+{
+	FItemStruct* ItemInfo = ItemDictionary.Find(ID);
+
+	if (ItemInfo == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("GetItemInfo : ItemInfo == nullptr"));
+		return nullptr;
+	}
+
+	return ItemInfo;
 }
