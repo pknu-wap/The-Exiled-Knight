645ca9fb93f4ef0e890e6376416e81a5dd71c207
hallow0312 <leejaehyun0312@naver.com>
2024-11-02 01:48:51 +0900

    Feat:: C++ AIcontroller

--- TheExiledKnight/Source/TheExiledKnight/Enemy/EKEnemyGamePlayTags.cpp
+++ TheExiledKnight/Source/TheExiledKnight/Enemy/EKEnemyGamePlayTags.cpp
@@ -4,12 +4,16 @@
 #include "EKEnemyGamePlayTags.h"
 namespace EKPlayerGameplayTags
 {
+#pragma region Status
+
 	UE_DEFINE_GAMEPLAY_TAG(EKEnemy_State_Idle, "EKEnemy.State.Idle");
 	UE_DEFINE_GAMEPLAY_TAG(EKEnemy_State_Walk, "EKEnemy.State.Walk");
 	UE_DEFINE_GAMEPLAY_TAG(EKEnemy_State_Sprint, "EKEnemy.State.Sprint");
-	UE_DEFINE_GAMEPLAY_TAG(EKEnemy_State_Attack, "EKEnemy.State.Attack");
+	UE_DEFINE_GAMEPLAY_TAG(EKEnemy_State_Combat, "EKEnemy.State.Combat");
 	UE_DEFINE_GAMEPLAY_TAG(EKEnemy_State_Chase, "EKEnemy.State.Chase");
 	UE_DEFINE_GAMEPLAY_TAG(EKEnemy_State_Stun, "EKEnemy.State.Stun");
+#pragma endregion
+
 
 
 }
