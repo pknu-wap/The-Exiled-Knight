6c6f6f143f39557cfeba5ee41157e8bd457cabc3
hallow0312 <leejaehyun0312@naver.com>
2024-11-06 00:24:12 +0900

    Feat: 방향에 따른 피격 판정 애니메이션 및 비헤이비어 트리 세분화

--- TheExiledKnight/Source/TheExiledKnight/Enemy/EK_EnemyBase.h
+++ TheExiledKnight/Source/TheExiledKnight/Enemy/EK_EnemyBase.h
@@ -5,7 +5,10 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "GameplayTagContainer.h"
+#include "BehaviorTree/BehaviorTree.h"
 #include "EK_EnemyBase.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnHitAnimationEnd);   
 
 UCLASS()
 class THEEXILEDKNIGHT_API AEK_EnemyBase : public ACharacter
@@ -16,15 +19,32 @@ public:
 	// Sets default values for this character's properties
 	AEK_EnemyBase(); 
 
-	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, AController* EventInvestigator, AActor* DmageCauser)override;
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, AController* EventInvestigator, AActor* DamageCauser)override;
 	
+	UPROPERTY(BlueprintAssignable, Category = "HitAnimation")
+	FOnHitAnimationEnd OnHurtAnimationEnd;  
+
+	void PlayHurtReactionAnimation(const FVector& DamageDirection);
+	void OnHurtMontageEnded(UAnimMontage* Montage, bool bInterrupted);
+	void PlayDieReactionAnimation();
+	
+
 protected:
 
 	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "AnimMontage", meta = (AllowPrivateAccess = "true"));
 	UAnimMontage* AttackMontage;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AnimMontage", meta = (AllowPrivateAccess = "true"));
-	UAnimMontage* hurtMontage;
+	UAnimMontage* hurtFMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AnimMontage", meta = (AllowPrivateAccess = "true"));
+	UAnimMontage* hurtLMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AnimMontage", meta = (AllowPrivateAccess = "true"));
+	UAnimMontage* hurtRMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AnimMontage", meta = (AllowPrivateAccess = "true"));
+	UAnimMontage* hurtBMontage;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AnimMontage", meta = (AllowPrivateAccess = "true"));
 	UAnimMontage* DeadMontage;
@@ -33,8 +53,9 @@ protected:
 public:	
 	UPROPERTY(VisibleAnywhere, Category = Stat)
 	class UEK_EnemyStatusComponent* EnemyStat;
-	
-	
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BehaviorTree");
+	UBehaviorTree* BehaviorTree;
 	
 	virtual TObjectPtr <UEK_EnemyStatusComponent> GetStatusComponent();
 	
