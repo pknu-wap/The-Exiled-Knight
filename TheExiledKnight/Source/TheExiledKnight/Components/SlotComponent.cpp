3cf9727087e9e914310a61c6b9238287c2faba58
HyeonJun-Jung <jhj123p@gmail.com>
2024-11-05 13:11:39 +0900

    Feat : Equip Logic 추가

--- TheExiledKnight/Source/TheExiledKnight/Components/SlotComponent.cpp
+++ TheExiledKnight/Source/TheExiledKnight/Components/SlotComponent.cpp
@@ -2,6 +2,9 @@
 
 
 #include "Components/SlotComponent.h"
+#include "UI/UISubsystem.h"
+#include "EKGameplayTags.h"
+#include "UI/Equipment/Widget_Equipment.h"
 
 // Sets default values for this component's properties
 USlotComponent::USlotComponent()
@@ -19,8 +22,10 @@ void USlotComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	WeaponSlots.Reset(2);
+	RuneSlots.Reset(4);
+	UsableSlots.Reset(4);
+	MagicSlots.Reset(6);
 }
 
 
@@ -30,5 +35,60 @@ void USlotComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorC
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+}
+
+void USlotComponent::EquipWeapon(const FItemStruct& InItemData)
+{
+	UWidget_Equipment* equipWidget = GetEquipmentWidget();
+	if (!equipWidget) return;
+
+	int slotIdx = equipWidget->GetEquipSelectSlotIdx();
+	if (WeaponSlots.IsValidIndex(slotIdx))
+	{
+		WeaponSlots[slotIdx] = InItemData;
+		Delegate_SlotUpdated.Broadcast(EEquipCategory::Weapon, slotIdx);
+	}
+}
+
+void USlotComponent::EquipRune(const FItemStruct& InItemData)
+{
+	UWidget_Equipment* equipWidget = GetEquipmentWidget();
+	if (!equipWidget) return;
+
+	int slotIdx = equipWidget->GetEquipSelectSlotIdx();
+	if (RuneSlots.IsValidIndex(slotIdx))
+	{
+		RuneSlots[slotIdx] = InItemData;
+		Delegate_SlotUpdated.Broadcast(EEquipCategory::Rune, slotIdx);
+	}
+}
+
+void USlotComponent::EquipUsableItem(const FItemStruct& InItemData)
+{
+	UWidget_Equipment* equipWidget = GetEquipmentWidget();
+	if (!equipWidget) return;
+
+	int slotIdx = equipWidget->GetEquipSelectSlotIdx();
+	if (UsableSlots.IsValidIndex(slotIdx))
+	{
+		UsableSlots[slotIdx] = InItemData;
+		Delegate_SlotUpdated.Broadcast(EEquipCategory::UsableItem, slotIdx);
+	}
+}
+
+void USlotComponent::EquipMagic(const FMagicStruct& InMagicData)
+{
+}
+
+UWidget_Equipment* USlotComponent::GetEquipmentWidget()
+{
+	UUISubsystem* UISystem = GetWorld()->GetGameInstance()->GetSubsystem<UUISubsystem>();
+	if (!UISystem) return nullptr;
+
+	UUserWidget* widget = UISystem->GetWidget(FEKGameplayTags::Get().UI_Widget_GameMenu_Equipment);
+	if (!widget) return nullptr;
+
+	UWidget_Equipment* equipWidget = Cast<UWidget_Equipment>(widget);
+	return equipWidget;
 }
 
