c477c1abe938e464fcd0aee0a9756efb194711b0
HyeonJun-Jung <jhj123p@gmail.com>
2024-11-05 13:11:39 +0900

    Feat : Equip Logic 추가

--- TheExiledKnight/Source/TheExiledKnight/UI/Equipment/Widget_Equipment.cpp
+++ TheExiledKnight/Source/TheExiledKnight/UI/Equipment/Widget_Equipment.cpp
@@ -10,8 +10,11 @@ void UWidget_Equipment::ShowEquipWindow()
 	WidgetSwitcher->SetActiveWidgetIndex(0);
 }
 
-void UWidget_Equipment::ShowEquipSelect(EEquipCategory Category)
+void UWidget_Equipment::ShowEquipSelect(EEquipCategory Category, int slotIdx)
 {
+	// Save Slot Idx
+	SlotIdx_EquipSelect = slotIdx;
+
 	// Update Contents by Category
 	EquipSelect->UpdateContents(Category);
 	WidgetSwitcher->SetActiveWidgetIndex(1);
