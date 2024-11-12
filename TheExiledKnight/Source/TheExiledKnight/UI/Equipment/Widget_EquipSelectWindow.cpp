aece5a8d7d7bd5b16169659c888341c8cf1ad831
HyeonJun-Jung <jhj123p@gmail.com>
2024-11-12 21:36:39 +0900

    Feat : 장비 착용 UI 데이터 연동 및 QuickSlot 데이터 연동

--- TheExiledKnight/Source/TheExiledKnight/UI/Equipment/Widget_EquipSelectWindow.cpp
+++ TheExiledKnight/Source/TheExiledKnight/UI/Equipment/Widget_EquipSelectWindow.cpp
@@ -3,6 +3,7 @@
 
 #include "UI/Equipment/Widget_EquipSelectWindow.h"
 #include "UI/Equipment/Widget_EquipSelect_ContentList.h"
+#include "Components/TextBlock.h"
 
 void UWidget_EquipSelectWindow::NativeConstruct()
 {
@@ -11,5 +12,35 @@ void UWidget_EquipSelectWindow::NativeConstruct()
 
 void UWidget_EquipSelectWindow::UpdateContents(EEquipCategory Category)
 {
+	switch (Category)
+	{
+	case EEquipCategory::None:
+	{
+		break;
+	}
+	case EEquipCategory::Weapon:
+	{
+		Text_Category->SetText(FText::FromString(FString("Weapon")));
+		break;
+	}
+	case EEquipCategory::Rune:
+	{
+		Text_Category->SetText(FText::FromString(FString("Rune")));
+		break;
+	}
+	case EEquipCategory::FragmentOfGod:
+	{
+		Text_Category->SetText(FText::FromString(FString("Fragment")));
+		break;
+	}
+	case EEquipCategory::UsableItem:
+	{
+		Text_Category->SetText(FText::FromString(FString("Usable Item")));
+		break;
+	}
+	default:
+		break;
+	}
+
 	ContentList->UpdateContents(Category);
 }
---
+++ TheExiledKnight/Source/TheExiledKnight/UI/QuickSlots/Widget_QuickSlots.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Widget_QuickSlots.generated.h"
+
+class UWidget_QuickSlot;
+
+UCLASS()
+class THEEXILEDKNIGHT_API UWidget_QuickSlots : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, EditAnywhere)
+	UWidget_QuickSlot* QuickSlot_Weapon;
+
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, EditAnywhere)
+	UWidget_QuickSlot* QuickSlot_Fragment;
+
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, EditAnywhere)
+	UWidget_QuickSlot* QuickSlot_Magic;
+
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, EditAnywhere)
+	UWidget_QuickSlot* QuickSlot_UsableItem;
+};
