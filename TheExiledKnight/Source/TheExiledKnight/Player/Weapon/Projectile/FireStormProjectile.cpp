87531e7d35c5751541314b9e3aeabc54ea820194
KaneBigNose <imddongs@naver.com>
2024-11-13 22:05:45 +0900

    Feat: 스태프 스킬 Type-A 구현

---
+++ TheExiledKnight/Source/TheExiledKnight/Player/Weapon/Projectile/FireStormProjectile.cpp
@@ -0,0 +1,81 @@
+// Made by Somalia Pirate
+
+#include "FireStormProjectile.h"
+
+AFireStormProjectile::AFireStormProjectile()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	StormMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StormMeshComponent"));
+	StormMeshComponent->SetupAttachment(RootComponent);
+	StormMeshComponent->SetRelativeScale3D(FVector(12.f, 12.f, 12.f));
+	StormMeshComponent->SetRelativeLocation(FVector(0, 0, -70));
+
+	CapsuleComponent->SetCapsuleHalfHeight(400.f);
+	CapsuleComponent->SetCapsuleRadius(100.f);
+	CapsuleComponent->SetRelativeLocation(FVector(0, 0, 0));
+
+	ProjectileMovementComponent->InitialSpeed = 100.f;
+	ProjectileMovementComponent->MaxSpeed = 100.f;
+
+	BaseParticle->SetRelativeLocation(FVector(0, 0, -80));
+
+	SetLifeSpan(12.f);
+}
+
+void AFireStormProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void AFireStormProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (bIsHitOnce)
+	{
+		return;
+	}
+
+	FVector CapsuleLocation = CapsuleComponent->GetComponentLocation();
+	FRotator CapsuleRotation = CapsuleComponent->GetComponentRotation();
+	float CapsuleHalfHeight = CapsuleComponent->GetScaledCapsuleHalfHeight();
+	float CapsuleRadius = CapsuleComponent->GetScaledCapsuleRadius();
+
+	TArray<FHitResult> HitResults;
+
+	bool bIsHit = this->GetWorld()->SweepMultiByChannel(
+		HitResults,
+		CapsuleLocation,
+		CapsuleLocation,
+		CapsuleRotation.Quaternion(),
+		ECC_Pawn,
+		FCollisionShape::MakeCapsule(CapsuleRadius, CapsuleHalfHeight)
+	);
+
+	if (!bIsHit)
+	{
+		return;
+	}
+
+	for (auto& Hit : HitResults)
+	{
+		AActor* HitActor = Hit.GetActor();
+		if (HitActor)
+		{
+			AEK_EnemyBase* HitEnemy = Cast<AEK_EnemyBase>(HitActor);
+			TSubclassOf<UEKPlayerDamageType> PlayerDamageType = UEKPlayerDamageType::StaticClass();
+			if (HitEnemy)
+			{
+				UGameplayStatics::ApplyDamage(HitEnemy, EKPlayer->GetPlayerStatusComponent()->GetPlayerFinalDamage(), EKPlayerController, EKPlayer->GetCurrentWeapon(), PlayerDamageType);
+				bIsHitOnce = true;
+				if (HitParticle)
+				{
+					UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), HitParticle, GetActorLocation(), GetActorRotation());
+				}
+				GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Purple, TEXT("Fire Storm"));
+			}
+		}
+	}
+}
