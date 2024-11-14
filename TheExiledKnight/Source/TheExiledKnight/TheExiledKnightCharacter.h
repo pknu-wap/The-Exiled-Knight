1d052e3ba2bdc06a52b2706e4f87bfe1805035f3
WWjj16 <lds062626@naver.com>
2024-11-14 23:18:04 +0900

    Fix : Test 맵에서 크래시나던 버그 수정
    BP_ThirdPersonCharacter.uasset을 제거하여 해결

--- TheExiledKnight/Source/TheExiledKnight/TheExiledKnightCharacter.h
+++ TheExiledKnight/Source/TheExiledKnight/TheExiledKnightCharacter.h
@@ -70,10 +70,5 @@ public:
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 	/** Returns FollowCamera subobject **/
 	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
-
-
-private:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	TObjectPtr<UInventoryComponent> InventoryComponent;
 };
 
