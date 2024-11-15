34a3ce7cee0f7907ad41012004fdcb6d8789ead9
KaneBigNose <imddongs@naver.com>
2024-11-15 14:35:02 +0900

    Feat: 스태프 스킬 Type-B 구현

--- TheExiledKnight/Source/TheExiledKnight/Player/Weapon/Projectile/ThunderingProjectile.h
+++ TheExiledKnight/Source/TheExiledKnight/Player/Weapon/Projectile/ThunderingProjectile.h
@@ -4,15 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "Player/Weapon/Projectile/EKPlayerProjectile.h"
-#include "ThunderProjectile.generated.h"
+#include "ThunderingProjectile.generated.h"
 
 UCLASS()
-class THEEXILEDKNIGHT_API AThunderProjectile : public AEKPlayerProjectile
+class THEEXILEDKNIGHT_API AThunderingProjectile : public AEKPlayerProjectile
 {
 	GENERATED_BODY()
 	
 public:	
-	AThunderProjectile();
+	AThunderingProjectile();
 
 protected:
 	virtual void BeginPlay() override;
