8ef5cf05c31f03b5c05acbd48f51b15eaab1844c
HyeonJun-Jung <jhj123p@gmail.com>
2024-11-18 21:44:58 +0900

    Feat : Equipment Upgrade 카테고리 구분 구현
    - Sword, Spear, Staff, Potion으로 현재 구분중

--- TheExiledKnight/Source/TheExiledKnight/UI/Upgrade/Equipment/Widget_UpgradeEquipment.h
+++ TheExiledKnight/Source/TheExiledKnight/UI/Upgrade/Equipment/Widget_UpgradeEquipment.h
@@ -5,17 +5,50 @@
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
 #include "Structs/InventorySlotStruct.h"
+#include "EKEnums.h"
 #include "Widget_UpgradeEquipment.generated.h"
 
-/**
- * 
- */
+class UButton;
+class UScrollBox;
+class UWidget_InvContentList;
+class UWidget_EU_CategorySlot;
+
 UCLASS()
 class THEEXILEDKNIGHT_API UWidget_UpgradeEquipment : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+protected:
+	virtual void NativeConstruct() override;
+
 public:
+	UFUNCTION(BlueprintCallable)
+	void UpdateContents(EUpgradeItemType WidgetType);
+
+public:
+	UFUNCTION()
+	void ShowBeforeCategory();
+
+	UFUNCTION()
+	void ShowNextCategory();
+
 	UFUNCTION(BlueprintImplementableEvent)
 	void UpdateDescription(const FInventorySlot& SlotData);
+
+public:
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, EditAnywhere)
+	UWidget_InvContentList* ContentList;
+
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, EditAnywhere)
+	UScrollBox* ScrollBox_Category;
+
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, EditAnywhere)
+	UButton* Button_BeforeCategory;
+
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly, EditAnywhere)
+	UButton* Button_NextCategory;
+
+private:
+	TArray<UWidget_EU_CategorySlot*> CategorySlots;
+	int CurrentCategoryIdx;
 };
