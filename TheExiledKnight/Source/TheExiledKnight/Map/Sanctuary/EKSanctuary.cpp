85d1bc930a4f9f1935e03546505f900475566240
HyeonJun-Jung <jhj123p@gmail.com>
2024-11-21 00:56:44 +0900

    Feat : 성역(생츄어리) 생성 및 구현
    - Interact는 아직 구현하지 않음
    - 기능 : 연관된 Actor들 Save & Load

--- TheExiledKnight/Source/TheExiledKnight/Map/Sanctuary/EKSanctuary.cpp
+++ TheExiledKnight/Source/TheExiledKnight/Map/Sanctuary/EKSanctuary.cpp
@@ -3,6 +3,8 @@
 
 #include "Map/Sanctuary/EKSanctuary.h"
 #include "NiagaraComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Enemy/EK_EnemyBase.h"
 
 // Sets default values
 AEKSanctuary::AEKSanctuary()
@@ -25,6 +27,8 @@ void AEKSanctuary::BeginPlay()
 	Super::BeginPlay();
 	
 	ActivateSantuary();
+
+	SaveMap();
 }
 
 // Called every frame
@@ -48,5 +52,72 @@ void AEKSanctuary::ActivateSantuary()
 			GetWorld()->GetTimerManager().ClearTimer(ActivateHandle);
 			// ActivateHandle.Invalidate();
 		}, 1, false);
+}
+
+void AEKSanctuary::SaveMap()
+{
+	// Save Enemy Transform
+	TArray<AActor*> EnemyArray;
+	UGameplayStatics::GetAllActorsOfClass(this, AEK_EnemyBase::StaticClass(), EnemyArray);
+	for (const auto actor : EnemyArray)
+	{
+		AEK_EnemyBase* enemy = Cast<AEK_EnemyBase>(actor);
+		if (!enemy) continue;
+
+		if (enemy->SantuaryID == SanctuaryID)
+		{
+			SavedActors.Add(actor);
+			SavedTransforms.Add(enemy->GetTransform());
+			SavedClasses.Add(enemy->GetClass());
+		}
+	}
+}
+
+void AEKSanctuary::LoadMap()
+{
+	// Load Enemy Transform
+	for (int i = 0; i < SavedActors.Num(); i++)
+	{
+		if (!SavedTransforms.IsValidIndex(i) || !SavedClasses.IsValidIndex(i))
+		{
+			continue;
+		}
+
+		const auto actor = SavedActors[i];
+		if (IsValid(actor))
+		{
+			AEK_EnemyBase* enemy = Cast<AEK_EnemyBase>(actor);
+
+			// Recover Start State
+
+			// Set Transform
+			enemy->StopAnimMontage();
+		}
+		else
+		{
+			AActor* spawnedActor = GetWorld()->SpawnActor<AActor>(SavedClasses[i]);
+			AEK_EnemyBase* enemy = Cast<AEK_EnemyBase>(spawnedActor);
+			if (!enemy)
+			{
+				UE_LOG(LogTemp, Warning, TEXT("AEKSanctuary : Failed to Spawn Saved Enemy."));
+			}
+
+			// Recover Start State
+
+			// Set Transform
+			enemy->SetActorTransform(SavedTransforms[i], false, nullptr, ETeleportType::ResetPhysics);
+		}
+	}
+
+	GetWorld()->GetTimerManager().SetTimer(Handle_Transform,
+		[&]() {
+			for (int i = 0; i < SavedActors.Num(); i++)
+			{
+				if (SavedActors[i])
+					SavedActors[i]->SetActorTransform(SavedTransforms[i], false, nullptr, ETeleportType::ResetPhysics);
+			}
+		},
+		0.5, false);
+
 }
 
