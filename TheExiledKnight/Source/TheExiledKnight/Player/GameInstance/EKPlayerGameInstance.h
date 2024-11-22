d0782a0a54d4c41248a609a3c2ea01c5dd65e99d
KaneBigNose <imddongs@naver.com>
2024-11-22 19:29:01 +0900

    Feat: 플레이어 데이터 관리용 CSV 추가 및 GameInstance 추가

---
+++ TheExiledKnight/Source/TheExiledKnight/Player/GameInstance/EKPlayerGameInstance.h
@@ -0,0 +1,33 @@
+// Made by Somalia Pirate
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "Player/Data/EKPlayerLevel.h"
+#include "Player/Data/EKPlayerStatus.h"
+#include "Engine/DataTable.h"
+#include "EKPlayerGameInstance.generated.h"
+
+UCLASS()
+class THEEXILEDKNIGHT_API UEKPlayerGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UEKPlayerGameInstance();
+
+protected:
+	virtual void Init();
+
+public:
+	FEKPlayerLevel* GetEKPlayerLevelData(int32 Level);
+	FEKPlayerStatus* GetEKPlayerStatusData(int32 Level);
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UDataTable* EKPlayerLevelData;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UDataTable* EKPlayerStatusData;
+};
